8d00c6cc54fa3ffa738971d756b9278e93231f7c
Lidiyaaa26 <lidiya6790@gmail.com>
2024-09-27 19:11:56 +0300

    Адаптивность, интерактивность сайта

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 const schemeSvg = document.querySelector('.scheme-svg');
 const totalPriceTag = document.querySelector(".price-total")
+const menuButton = document.querySelector(".m-menu")
+const menu = document.querySelector(".menu")
 let cost = 500;
 let totalPrice = 0;
+const playButton = document.querySelector('.film-trailer');
+
 schemeSvg.addEventListener('click', (event) => {
     if(!event.target.classList.contains("booked")) {
         event.target.classList.toggle("active")
@@ -10,3 +14,18 @@ schemeSvg.addEventListener('click', (event) => {
         totalPriceTag.textContent = totalPrice;
     }
 });
+
+menuButton.addEventListener("click", () => {
+    console.log("Кликнули по меню");
+    menu.classList.toggle("is-open");
+});
+playButton.addEventListener("click", () => {
+    Fancybox.show([
+        {
+          src: "https://youtu.be/6ZfuNTqbHE8?si=SgA6hA_iBGWJx6QR",
+          width: 640,
+          height: 360,
+        },
+    ]);
+});
+
